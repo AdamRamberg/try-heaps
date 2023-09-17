3c090ed7be3f17c4bbea1e688a81b20241309218
Adam Ramberg <adam@mambojambostudios.com>
2023-09-17 18:35:42 +0200

    Add SampleView

--- src/Main.hx
+++ src/Main.hx
@@ -1,9 +1,22 @@
 class Main extends hxd.App {
     override function init() {
-        var tf = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
-        tf.text = "Hello Hashlink !";
+        var view = new SampleView(h2d.Tile.fromColor(0xFF,32,32), s2d);
+        view.mybmp.alpha = 0.8;
     }
     static function main() {
         new Main();
+    }
+}
+
+class SampleView extends h2d.Flow implements h2d.domkit.Object {
+    static var SRC = 
+        <sample-view layout="vertical"> 
+            Hello World! 
+            <bitmap src={tile} public id="mybmp"/>
+        </sample-view>
+
+    public function new(tile:h2d.Tile,?parent) {
+        super(parent);
+        initComponent();
     }
 }
