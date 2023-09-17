a1f42294c0abeb6c057aeb475350413ee2e3ce18
Adam Ramberg <adam@mambojambostudios.com>
2023-09-17 23:53:26 +0200

    Fix compile errors in Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -8,10 +8,11 @@ class Main extends hxd.App {
     }
 }
 
+@:uiComp("sample-view")
 class SampleView extends h2d.Flow implements h2d.domkit.Object {
     static var SRC = 
         <sample-view layout="vertical"> 
-            Hello World! 
+            <text text={"Hello World"}/>
             <bitmap src={tile} public id="mybmp"/>
         </sample-view>
 
